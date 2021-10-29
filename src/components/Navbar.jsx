fc902ad2299f67b534ebc4b2e31e043a7409a6b7
Gbemisola Kotoye <gkotoye@gmail.com>
2021-10-29 15:20:08 +0100

    Update Clone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import './Navbar.css';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+	const [show, handleShow] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener("scroll", () => {
+			if(window.scrollY > 100) {
+				handleShow(true);
+			} else handleShow(false)
+		});
+		return () => {
+			window.removeEventListener("scroll");
+		}
+	}, [])
 
     return (
-	<div className="nav">
-	   <img className="nav_logo" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fthereelbits.com%2Fwp-content%2Fuploads%2F2017%2F05%2FNetflix-Logo.jpg&imgrefurl=https%3A%2F%2Fthereelbits.com%2F2017%2F05%2F16%2Fnetflix-announces-tidelands-its-first-australian-original-series%2F&tbnid=UxbKkWLVWeKjsM&vet=1&docid=tfATyjoBKIC1uM&w=960&h=472&itg=1&source=sh%2Fx%2Fim" alt="Netflix logo"/>
-	    <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Netflix avatar"/>
+	<div className={`nav ${show && "nav_black"}`}>
+	    <img className="nav_logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix logo"/>
+		<img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Netflix avatar"/>
 	</div>
     )
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import axios from '../axios'
 import "./Row.css";
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
+
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
@@ -15,7 +19,30 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
         fetchData();
     }, [fetchUrl])
 
-    console.log(movies);
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "")
+            .then((url) => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        }
+    };
+
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -23,12 +50,14 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
                 {movies.map((movie) => (
                     <img
                     key={movie.id}
+                    onClick={() => handleClick(movie)}
                     className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                     src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                     alt={movie.name}
                     />
                 ))}
             </div>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
         </div>
     )
 }
